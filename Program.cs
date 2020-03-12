cc308661bd31b65ef452fce11de8a277de764cd5
Gopal Vashishtha <gopalv@microsoft.com>
2020-03-11 17:16:59 -0700

    publishing project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IowaHouseML;
+using IowaHouseML.ConsoleApp;
+
+namespace IowaHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModelBuilder.CreateModel(TRAIN_DATA_FILEPATH: args[0], MODEL_FILEPATH: args[1]);
+        }
+    }
+}
